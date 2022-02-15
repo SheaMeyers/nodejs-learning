0fc0ba16e92f7396286b69525714c5f826f348e3
Shea Meyers <shea.meyers@gmail.com>
2022-02-15 19:16:08 +0100

    Implement adding and removing notes using json

--- complete-developer-course/notes-app/notes.js
+++ complete-developer-course/notes-app/notes.js
@@ -1,5 +1,55 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
 const getNotes = function() {
     return "Your notes..."
 }
 
-module.exports = getNotes
+const addNote = function (title, body) {
+    const notes = loadNotes()
+    const duplicateNotes = notes.filter(function (note) {
+        return note.title === title
+    })
+
+
+    if (duplicateNotes.length === 0) {
+        notes.push({
+            title,
+            body
+        })
+        saveNotes(notes)        
+        console.log('New note added!')
+    } else {
+        console.log('Note title taken!')
+    }
+}
+
+const removeNote = function (title) {
+    const notes = loadNotes()
+    const filteredNotes = notes.filter(note => note.title !== title)
+    if (notes.length !== filteredNotes.length) {
+        saveNotes(filteredNotes)
+        console.log(chalk.green('Note removed!'))
+    } else {
+        console.log(chalk.red('No note found!'))
+    }
+}
+
+const saveNotes = function (notes) {
+    const dataJSON = JSON.stringify(notes)
+    fs.writeFileSync("notes.json", dataJSON)
+}
+
+const loadNotes = function () {
+    try {
+      const dataBuffer = fs.readFileSync("notes.json");
+      const dataJSON = dataBuffer.toString();
+      return JSON.parse(dataJSON);
+    } catch (e) {
+      return [];
+    }
+}
+
+module.exports = {
+    addNote, getNotes, removeNote
+}
