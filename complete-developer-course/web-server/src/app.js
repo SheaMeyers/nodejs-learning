2095f58e70c69d3360ef7599ca4c387e0ab2cb8f
Shea Meyers <shea.meyers@gmail.com>
2022-03-06 18:00:56 +0100

    Use templating for displaying information

--- complete-developer-course/web-server/src/app.js
+++ complete-developer-course/web-server/src/app.js
@@ -1,12 +1,42 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 
-app.use(express.static(path.join(__dirname, '../public')))
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
-    res.send('<h1>Weather</h1>')
+    res.render('index', {
+        title: 'Weather',
+        name: 'Shea Meyers'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Shea Meyers'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'This is some helpful text.',
+        title: 'Help',
+        name: 'Shea Meyers'
+    })
 })
 
 app.get('/weather', (req, res) => {
@@ -16,6 +46,22 @@ app.get('/weather', (req, res) => {
     })
 })
 
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Shea Meyers',
+        errorMessage: 'Help article not found.'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Shea Meyers',
+        errorMessage: 'Page not found.'
+    })
+})
+
 app.listen(3000, () => {
-    console.log('Server is up on port 3000')
+    console.log('Server is up on port 3000.')
 })
