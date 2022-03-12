0f05bae220116ab97b0a462d79b09c9340443a78
Shea Meyers <shea.meyers@gmail.com>
2022-03-12 15:37:53 +0100

    Get code working to work on both localhost and heroku

--- complete-developer-course/web-server/public/js/app.js
+++ complete-developer-course/web-server/public/js/app.js
@@ -3,7 +3,7 @@ const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
 
-const getData = (location) => fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+const getData = (location) => fetch(`${window.location.href}weather?address=${location}`).then((response) => {
     response.json().then((data) => {
         if (data.error) {
             messageOne.textContent = data.error
