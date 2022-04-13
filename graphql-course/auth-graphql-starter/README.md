8c9bc2021a2cdc0a86b630742b2a4453949300f4
Shea Meyers <shea.meyers@gmail.com>
2022-04-13 18:22:19 +0200

    Complete auth project for graphql

---
+++ graphql-course/auth-graphql-starter/README.md
@@ -0,0 +1,8 @@
+# auth-graphql-starter
+Starter project from a GraphQL course on Udemy.com - Section 3!
+
+Run project using
+
+```
+npm run dev
+```
---
+++ graphql-course/auth-graphql-starter/client/components/AuthForm.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+
+class AuthForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { email: '', password: '' };
+    }
+
+    onSubmit(event) {
+        event.preventDefault();
+
+        this.props.onSubmit(this.state);
+    }
+
+    render() {
+        return (
+            <div className='row'>
+                <form onSubmit={this.onSubmit.bind(this)} className='col s4'>
+                    <div className='input-field'>
+                        <label>Email</label>
+                        <input 
+                            value={this.state.email}
+                            placeholder="Email"
+                            type="email"
+                            onChange={e => this.setState({ email: e.target.value })}
+                        />
+                    </div>
+                    <div className='input-field'>
+                        <label>Password</label>
+                        <input 
+                            value={this.state.password}
+                            placeholder="Password"
+                            type="password"
+                            onChange={e => this.setState({ password: e.target.value})}
+                        />
+                    </div>
+                    <div className='errors'>
+                        {this.props.errors.map(error => <div key={error}>{error}</div>)}
+                    </div>
+                    <button className='btn'>Submit</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default AuthForm;
